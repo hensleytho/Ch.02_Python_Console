159c4c244349277c95c9162455ecd6f2e5aa61e8
hensleytho <hensleytho@urbandaleschools.com>
2022-09-09 08:59:33 -0500

    Python Practice

---
+++ Python Practice Log.py
@@ -0,0 +1,42 @@
+# This is my pythin learning log
+
+#  this is a comment that does not affect the code
+
+''' woah '''
+
+print("Hello World")
+print(10.1//2) # floor division, // will cut off everything behind the decimal
+print(7%2) # modulus get the remainder
+print(abs(-7)) # abs - - absolute value
+
+a=2
+b=7
+print(a*b)
+print(b**a)
+print() # print with nothing in parenthesis leaves blank line when code is run
+print(b%2)
+print()
+
+a,b=("I'm Oded","I'm Tommy") # assigns multiple varibles at once
+print(a)
+print(b)
+
+x=8
+print(x)
+x+=1      # doing x+=1 is the same as x=x+1
+print(x)
+x*=2    # same as x=x*2
+print(x)
+
+bulletShot=0
+bulletShot+=3
+print(bulletShot)
+
+a = "text"
+b=3
+c=4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
