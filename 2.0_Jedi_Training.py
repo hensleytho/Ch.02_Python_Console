5138e5c7657561549719e985576ea8300f984269
hensleytho <hensleytho@urbandaleschools.com>
2022-09-09 09:22:07 -0500

    Finished python practice

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,47 +1,49 @@
 '''
-Sign your name:________________
+Sign your name:__Tommy Hensley__
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+use a hashtag #it will not affect the code
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
-
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
-
+'''
+a=2
+b=5
+print(b/a)
+print(b//a)
+print(b**a)
+print(b%a)
+#print(a+B) B is undefined
+print(type(42))
+print(type(42.0))
+print(type("C3PO"))
+print(type(True))
+'''
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
-
+'''
 a=2
 a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
-
+print(a) # 15.0
+print(type(a))
+'''
 
 4.) What is the mistake in the following code. Fix it!
-
-x,y = (4,5)
-a = 3(x + y)
+'''
+x,y = 4,5 # do not need parenthesis
+a = 3*(x + y) # need a multiplication sign behind the 3
 a
-
+'''
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
-
+'''
 x,y,z =(3,4,5)
 ave = x+y+z/3
-ave
-
+print(ave) # needed the print in front of ave
+'''
 
 '''
